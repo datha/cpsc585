95e5ef4c86f2cac7b3a9a6e5e78393280fd1b67e
athadj <atha.dj@gmail.com>
2015-10-20 20:19:44 -0700

    working network and preprocess

--- network.py
+++ network.py
@@ -4,38 +4,119 @@ from sknn.platform import gpu32
 import sys
 import logging
 import pickle
+import argparse
+from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import classification_report
+
+import matplotlib.pyplot as plt
+
+import csv
+
+parser = argparse.ArgumentParser(description='Train network.')
+parser.add_argument('action', metavar='N', type=str,
+                    help='[train, predict]')
+parser.add_argument('--loadfile', dest='loadfile', action='store',
+                    type=str,
+                    help='pkl file to load.')
+
+args = parser.parse_args()
+
+class_mapper = {'irish': 0, 'mexican': 1, 'chinese': 2,
+                'filipino': 3, 'vietnamese': 4, 'spanish': 13, 'japanese': 7,
+                'moroccan': 5, 'french': 12, 'greek': 9, 'indian': 10,
+                'jamaican': 11, 'british': 8, 'brazilian': 6, 'russian':
+                14, 'cajun_creole': 15, 'thai': 16, 'southern_us': 17,
+                'korean': 18, 'italian': 19}
+
+#reverse mapping from string to int
+class_mapper = dict(zip(class_mapper.values(), class_mapper.keys()))
 
 
 
 def main():
-
+    NUMPY_INPUT_FILE_TEST = 'nn_test.npy'
+    NUMPY_INPUT_FILE = 'nn_train_39774.npy'
+    #NUMPY_INPUT_FILE = 'nn_train_140000.npy'
     logging.basicConfig(
-            format="%(message)s",
-            level=logging.DEBUG,
-            stream=sys.stdout)
-    data = np.load('nn_input.npy')
-    #print data
+        format="%(message)s",
+        level=logging.DEBUG,
+        stream=sys.stdout)
 
-    uid, y_train, X_train = data[0,:].transpose(), data[1,:].transpose(), data[2:,:].transpose()
+    data = np.load(NUMPY_INPUT_FILE)
+    data_test = np.load(NUMPY_INPUT_FILE_TEST)
+    print "Total Train Samples %d" % len(data.transpose())
+    print "Total Test Samples %d" % len(data_test.transpose())
+    uid, y_train, X_train = data[
+        0,:].transpose(), data[1,:].transpose(), data[2:,:].transpose()
+    uid_test,  X_test = data_test[
+        0,:].transpose(), data_test[1:,:].transpose()
+    train_index = int(X_train.shape[0] * .8)
+    print "Using %d samples to train" % train_index
+
+    # X_train, y_train, X_valid, y_valid = X_train[:train_index],
+    # y_train[:train_index], X_train[train_index:], y_train[train_index:]
+
+    # The most basic method of hyper paramater search
+    # learning reate, batch size
     nn = Classifier(
         layers=[
-            Layer("Maxout", name="h1", units=200, pieces=2,dropout=.1),
-            Layer("Maxout", name="h2", units=100, pieces=2,dropout=.1),
+            Layer("Maxout", units=450, pieces=2),  # ,dropout=.1),
+            # Layer("Tanh",units=300),#,dropout=.1),
             Layer("Softmax")],
-        learning_rate=0.001,
-        n_iter=8)
+        learning_rate=0.1,
+        # weight_decay=.0001,
+        dropout_rate=.05,
+        # random_state= ,
+        learning_momentum=.5,
+        valid_size=.2,
+        batch_size=100,
+        n_stable=5,
+        f_stable=.001,
+        # valid_set=(X_valid,y_valid),
+        n_iter=60)
+    if args.loadfile:
+        gs = pickle.load(open(args.loadfile, 'rb'))
+    else:
 
-    print "Trying to fit data"
-    train_index = int(X_train.shape[0] * .8)
-    try:
-        nn.fit(X_train[:train_index], y_train[:train_index])
-    except KeyboardInterrupt:
-        pass
-    #nn = pickle.load(open('nn.pkl', 'rb'))
-    pickle.dump(nn, open('nn.pkl', 'wb'))
+        gs = GridSearchCV(nn, cv=3, scoring='accuracy', param_grid={
+            'hidden0__units': [450,600],
+            #'hidden0__units': [150,300,450],
+            'hidden0__type': ["Maxout"],
+            'hidden0__pieces': [2],
+            'learning_rate': [0.05],
+            'batch_size': [100],
+            'dropout_rate': [.05]})
+    if args.action == 'train':
+        try:
 
-    print('score =', nn.score(X_train[train_index:], y_train[train_index:]))
-    #np.savetxt('x_train.out', x_train[:10], delimiter=',',fmt='%i')
-    #np.savetxt('y_train.out', y_train[:10], delimiter=',',fmt='%i')
+            gs.fit(X_train, y_train)
+            #nn.fit(X_train, y_train)
+
+            print gs.grid_scores_
+            print gs.best_score_
+            print gs.best_params_
+            
+            nn_filename = 'nnout/nn_%s.pkl' % 1001
+            gs_filename = 'nnout/gs_%s.pkl' % 1001
+
+            pickle.dump(nn, open(gs_filename, 'wb'))
+            pickle.dump(gs, open(nn_filename, 'wb'))
+
+            print "Saving %s" % nn_filename
+            print "Saving %s" % gs_filename
+
+        except KeyboardInterrupt:
+            pickle.dump(gs, open('gs_temp.pkl', 'wb'))
+            pickle.dump(nn, open('nnout/nn_%s.pkl' % 'trained', 'wb'))
+            pass
+    elif args.action == 'predict':
+        y_pred = gs.predict(X_test)
+        y_pred_mapped = [class_mapper[y] for y in y_pred.flat]
+        results = np.append([uid_test.astype(str)], [y_pred_mapped], axis=0)
+        print "Saving test.csv"
+        np.savetxt("test.csv", results.transpose(), "%s,%s", header="id,cuisine")
+
+
+
 if __name__ == "__main__":
     main()
--- preprocess.py
+++ preprocess.py
@@ -9,26 +9,28 @@ import numpy as np
 from sklearn.feature_extraction import DictVectorizer
 import sys
 import pickle
-from collections import Counter
-
+import copy
 from random import shuffle
+
+
 class Recipe(object):
 
-    def __init__(self, uid, cuisine, ingredients):
+    def __init__(self, uid=None, cuisine=None, ingredients=[]):
         self.uid = uid
         self.cuisine = cuisine
         self.ingredients = ingredients
 
     def get_int_ingredients(self, feature_dict):
-        #array returned will be formatted as follows:
+        # array returned will be formatted as follows:
         #[ingredient0, ingredient1 ... ingredientn]
         return [feature_dict[ingredient] for ingredient in self.ingredients]
-    def get_int_cuisine(self,cuisine_dict):
-        return cuisine_dict[self.cuisine] 
+
+    def get_int_cuisine(self, cuisine_dict):
+        return cuisine_dict[self.cuisine]
 
 # will used to remove certain words in order to reduce feature space
 black_list = ["whole", "fat", "reduced", "low", "crushed", "fine", "fresh",
-              "ground", "less", "chopped", "nonfat", "lowfat", "large", "grated", "sodium", "lowsodium", "free", "lean", "no", "solid", "cooking", "tips"]
+              "ground", "less", "chopped", "nonfat", "lowfat", "large", "grated", "sodium", "lowsodium", "free", "lean", "no", "solid", "cooking", "tips", "kraft", "fresh", "frozen", "chopped", "oz", "boneless", "skinless", "tastethai", "barilla", "bertolli", "bestfoods", "campbells", "lowfat", "crisco pure", "crystal farms" "reduced fat", "delallo", "domino", "heinz", "herdez", "hiddenvalleyoriginal", "johnsonville", "and", "fat free", "reducedsodium", "lowsodium", "lowersodium"]
 
 
 def intersect(a, b):
@@ -76,11 +78,18 @@ def reduce_to_single_word(words=[]):
 def main():
 
     recipes = []
+    recipes_test = []
     # If true uses blacklist to remove words
-    REMOVE_WORDS = False
+    REMOVE_WORDS = True
     REDUCE_TO_SINGLE = True
     CLEAN = True
     GET_UNIFORM = True
+    #artifically inflate data size by using duplicate data
+    GET_DUPLICATE = True
+    # Multiplied the the class count of the class with the least number of
+    # occurances.
+    NUMBER_OF_SAMPLES_PER_CLASS = 7000
+
     features = []
     classes = []
     # used to count occurances of each ingrediant and cuisine type
@@ -102,52 +111,76 @@ def main():
             features += recipe['ingredients']
             recipes.append(Recipe(uid, cuisine, recipe['ingredients']))
 
-    print "Total number of samples: %d" % len(recipes)
-    for cl in classes:
-        class_total_cnt[cl] += 1
+    with open('test.json') as f:
+        recipes_json = json.loads(f.read())
 
+        for recipe in recipes_json:
+            uid = int(recipe['id'])
+            features += recipe['ingredients']
+            recipes_test.append(
+                Recipe(uid=uid, ingredients=recipe['ingredients']))
+    print "Total number of samples: %d" % len(recipes)
+    print "Total number of test samples %d" % len(recipes_test)
+    # the recipes are shuffled each time. This way we can get different
+    # training subsets from the larager data set
     shuffle(recipes)
+
     label = 0
     for c in Set(classes):
         class_map[c] = label
         label += 1
     print class_map
 
-    #clean recipes
+    # clean recipes
     if CLEAN:
         features = clean(features)
         for recipe in recipes:
-            recipe.ingredients= clean(recipe.ingredients)
+            recipe.ingredients = clean(recipe.ingredients)
+        for recipe in recipes_test:
+            recipe.ingredients = clean(recipe.ingredients)
 
     if REDUCE_TO_SINGLE:
         print "Reducing to single"
-        features = Set(reduce_to_single_word( ))
+        features = Set(reduce_to_single_word(features))
         for recipe in recipes:
             recipe.ingredients = Set(reduce_to_single_word(recipe.ingredients))
+        for recipe in recipes_test:
+            recipe.ingredients = Set(reduce_to_single_word(recipe.ingredients))
+
     if REMOVE_WORDS:
         print "Removing words"
         features = Set(remove_words(features, black_list))
+        for recipe in recipes:
+            recipe.ingredients = Set(
+                remove_words(recipe.ingredients, black_list))
+        for recipe in recipes_test:
+            recipe.ingredients = Set(
+                remove_words(recipe.ingredients, black_list))
 
     if GET_UNIFORM:
-        #get least number fo samples
-        c, least = class_total_cnt.most_common()[-1]
-        number_per_class = least*3
-        print number_per_class
         bins = defaultdict(list)
         for recipe in recipes:
             bins[class_map[recipe.cuisine]].append(recipe)
-        
+
         recipes = []
         for key in bins.keys():
-            recipes = recipes + bins[key][:number_per_class]
+            class_count = len(bins[key])
+            while GET_DUPLICATE and (class_count < NUMBER_OF_SAMPLES_PER_CLASS):
+                print "Adding [%d] more recipes to [%s]" % (class_count, key)
+                bins[key] += copy.copy(bins[key])
+                class_count = len(bins[key])
+            # print len(bins[key])
+            # for i in range(len(bins[key]) % NUMBER_OF_SAMPLES_PER_CLASS):
+            recipes = recipes + bins[key][:NUMBER_OF_SAMPLES_PER_CLASS]
 
-    #print recipes[0]
+    # print recipes[0]
     shuffle(recipes)
-    print "Numober of samples %d" % len(recipes)
-
+    print "Numober of train samples %d" % len(recipes)
+    print "Numober of test samples %d" % len(recipes_test)
     feature_map = []
-    y_train =[]
-    labels =[]
+    y_train = []
+    labels = []
+
     for recipe in recipes:
         ing_d = dict()
         labels.append(recipe.uid)
@@ -156,17 +189,37 @@ def main():
             ing_d[ingredient] = ingredient
         feature_map.append(ing_d)
 
-    x_vec = DictVectorizer(dtype=np.int)
+    labels_test = []
+    feature_map_test = []
+    for recipe in recipes_test:
+        ing_d = dict()
+        labels_test.append(recipe.uid)
+        for ingredient in recipe.ingredients:
+            ing_d[ingredient] = ingredient
+        feature_map_test.append(ing_d)
 
-    X_train = x_vec.fit_transform(feature_map).toarray()
+    vec = DictVectorizer(dtype=np.int)
 
+    X_train = vec.fit_transform(feature_map).toarray()
+    X_test = vec.transform(feature_map_test).toarray()
+
+    print "num train features : %d" % len(X_train.transpose())
+    print "num test features : %d" % len(X_test.transpose())
     y_train = np.asarray(y_train)
     labels = np.asarray(labels)
-    label_y = np.append([labels],[y_train],axis=0)
-    #I think this still keesp the data lined up..need to double check
-    all_data = np.append(label_y,X_train.transpose(),axis=0)
-    #print all_data
-    np.save('nn_input', all_data)
-    #print y_train
+    labels_test = np.asarray(labels_test)
+
+    label_y = np.append([labels], [y_train], axis=0)
+
+    all_data = np.append(label_y, X_train.transpose(), axis=0)
+    all_data_test = np.append([labels_test], X_test.transpose(), axis=0)
+    # print all_data
+    train_file_name = 'nn_train_%d' % len(recipes)
+    test_file_name = 'nn_test' 
+    np.save(train_file_name, all_data)
+    np.save(test_file_name, all_data_test)
+    print "Saving %s" % train_file_name
+    print "Saving %s" % test_file_name
+    # print y_train
 if __name__ == "__main__":
     main()
